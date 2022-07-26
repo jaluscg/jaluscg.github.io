523c110d804f5b053be97341751dcf3d93411535
jaluscg <82360283+jaluscg@users.noreply.github.com>
2022-07-26 14:25:38 -0500

    Create x_pie.php

---
+++ x_pie.php
@@ -0,0 +1,24 @@
+<footer class="footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 offset-lg-2">
+                        <div class="widget">
+                            <div class="footer-text text-center">
+                                <a href="index.html"><img src="images/version/logonegro2.png" alt="" class="img-fluid"></a>
+
+                                <hr class="invis">
+
+                                    </form>
+                                </div><!-- end newsletter -->
+                            </div><!-- end footer-text -->
+                        </div><!-- end widget -->
+                    </div><!-- end col -->
+                </div>
+                <div class="row">
+                    <div class="col-md-12 text-center">
+                        <br>
+                        <br>
+                        <div class="copyright">&copy; Forest Time. Design: <a href="http://html.design">HTML Design</a>.</div>
+                    </div>
+                </div>
+            </div><!-- end container -->
