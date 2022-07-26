b0cb871a101566fbc352858fdb76e67527d05581
jaluscg <82360283+jaluscg@users.noreply.github.com>
2022-07-26 14:22:24 -0500

    Create x_menu.php

---
+++ x_menu.php
@@ -0,0 +1,50 @@
+<div class="topbar-section">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-4 col-md-6 col-sm-6 hidden-xs-down">
+                        <div class="topsocial">
+                        </div><!-- end social -->
+                    </div><!-- end col -->
+
+                    <div class="col-lg-4 hidden-md-down">
+                    </div><!-- end col -->
+
+                    <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
+                        <div class="topsearch text-right">
+                        </div><!-- end search -->
+                        </div><!-- end col -->
+                        </div><!-- end row -->
+                        </div><!-- end header-logo -->
+                        </div><!-- end topbar -->
+
+                                    <div class="header-section">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="logo">
+                                        <a href="index.php"><img src="images/version/logo.png" alt=""></a>
+                                    </div><!-- end logo -->
+                                </div>
+                            </div><!-- end row -->
+                        </div><!-- end header-logo -->
+                    </div><!-- end header -->
+
+    
+                <nav class="navbar navbar-inverse navbar-toggleable-md">
+                    <div class="collapse navbar-collapse justify-content-md-center" id="Forest Timemenu">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link color-green-hover" href="index.php">Inicio</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link color-green-hover" href="aprende.php">Aprende</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link color-green-hover" href="perfiles.php">Perfiles</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link color-green-hover" href="reproduccion.php">Listas de reproducción</a>
+                            </li>  
+                        </ul>
+                    </div>
+                </nav>
