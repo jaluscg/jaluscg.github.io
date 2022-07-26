289b871cd52b5a0cf48cee753a3a2039eebe3f48
jaluscg <82360283+jaluscg@users.noreply.github.com>
2022-07-26 14:21:05 -0500

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF=8"?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtmll/DTD/xhtmll=strict.dtd">
+
+
+<html  xmlns="http://www.w3.org/1999/xhtml" xml:lang="es" lang="es">
+  <head>
+   <title>Editorial Scan_Magico</title>
+  <meta http-equiv="Content-Type" Content="text/html; charset=UTF=8" />
+  <meta name="author" content="Jalus" />
+  <meta name="description" content="Editorial Scan_Magico" />
+  <meta name="keywords" content="wiki, novelas chinas, editorial" />
+ 
+   <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
+        <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+        <link href="https://fonts.googleapis.com/css?family=Droid+Sans:400,700" rel="stylesheet"> 
+        <link href="css/bootstrap.css" rel="stylesheet">
+        <link href="css/font-awesome.min.css" rel="stylesheet">
+        <link href="style.css" rel="stylesheet">
+        <link href="css/responsive.css" rel="stylesheet">
+        <link href="css/colors.css" rel="stylesheet">
+        <link href="css/version/garden.css" rel="stylesheet">
+
+   </head>
+    <body>        
+    
+        <?php include ('x_menu.php'); ?> 
+
+
+                    
+
+
+        <?php include ('x_pie.php'); ?> 
+
+    </body>
+</html>
+  
+
+ 
+ 
+ 
+
+
